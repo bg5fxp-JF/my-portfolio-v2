9a4d72ff224988876b0fb6a6f4430a03c9386226
bg5fxp-JF <jacobfecunda@outlook.com>
2024-01-30 19:41:08 +0000

    added resume section

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,14 @@
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 
+export const attributesGroup1 =
+	"Innovative · Adaptive · Collaborative · Proactive · Problem-solver · Tech-savvy · Boundary-pushing · Committed · Forward-thinking · Lifelong-learner";
+
+export const attributesGroup2 =
+	"Web3-proficient · Full-stack-expertise · Challenge-driven · Team-player · Solution-oriented · Creative-thinker · Passionate · Skilled-communicator · Ambitious · Resilient";
+
+export const attributesGroup3 =
+	"Motivated · Visionary · Strategic · Agile · Persistent · Empowering · Enthusiastic · Dedicated · Resourceful · Inspirational";
+
 export const myInfo = {
 	name: "Jacob Fecunda",
 	title: "Full-stack Web3 Developer",
@@ -39,4 +48,71 @@ export const navItems = [
 		title: "Contact",
 		link: "/contact",
 	},
+];
+export const clients = [
+	{
+		company: "Crypt0Hackers",
+		client: "Amaechi Okolobi",
+		img: "/images/ao.png",
+	},
+	{ company: "DeCentral Hub", client: "Dolapo", img: "/images/dch.jpg" },
+	{
+		company: "Magnate Core",
+		client: "Denis Kursevicius",
+		img: "/images/mc.png",
+	},
+];
+
+export const education = [
+	{
+		school: "Brunel University London",
+		qualification: "Bsc (Hons) Computer Science (AI) – 1st Class",
+		date: "(2020 - 2023)",
+		desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet, deleniti debitis a porro neque atque fugiat nemo provident hic! Ratione deserunt tempora fugit praesentium doloribus consequatur. Nisi non distinctio optio.",
+	},
+	{
+		school: "City and Islingtion College",
+		qualification: "Level 3 Software Developemnt Diploma - Dist* Dist* Dist*",
+		date: "(2018 - 2020)",
+		desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet, deleniti debitis a porro neque atque fugiat nemo provident hic! Ratione deserunt tempora fugit praesentium doloribus consequatur. Nisi non distinctio optio.",
+	},
+];
+
+export const workExperience = [
+	{
+		role: "Front-end Developer",
+		company: "Freenlance",
+		date: "(2022 - Present)",
+		desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet, deleniti debitis a porro neque atque fugiat nemo provident hic! Ratione deserunt tempora fugit praesentium doloribus consequatur. Nisi non distinctio optio.",
+	},
+	{
+		role: "Junior Software Developer",
+		company: "Otermans Institute",
+		date: "(Jun 2021 - Aug 2021)",
+		desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet, deleniti debitis a porro neque atque fugiat nemo provident hic! Ratione deserunt tempora fugit praesentium doloribus consequatur. Nisi non distinctio optio.",
+	},
+	{
+		role: "Software Developer (Intern)",
+		company: "ARN.",
+		date: "(2020 - 2021)",
+		desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet, deleniti debitis a porro neque atque fugiat nemo provident hic! Ratione deserunt tempora fugit praesentium doloribus consequatur. Nisi non distinctio optio.",
+	},
+];
+
+export const skills = [
+	{ skill: "HTML", level: "85" },
+	{ skill: "CSS", level: "80" },
+	{ skill: "Javascript", level: "83" },
+	{ skill: "Typescript", level: "60" },
+	{ skill: "React JS", level: "90" },
+	{ skill: "Next JS", level: "90" },
+	{ skill: "Bootstrap CSS", level: "85" },
+	{ skill: "Tailwind CSS", level: "95" },
+	{ skill: "SQL", level: "70" },
+	{ skill: "Solidity", level: "75" },
+	{ skill: "Hardhat", level: "75" },
+	{ skill: "Foundry", level: "70" },
+	{ skill: "Java", level: "85" },
+	{ skill: "Python", level: "80" },
+	{ skill: "Git & Github", level: "100" },
 ];
